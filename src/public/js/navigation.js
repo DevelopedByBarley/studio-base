16c59811afe0a005ed57699462140b0a43856296
Árpád <developedbybarley@gmail.com>
2022-06-09 19:40:00 +0200

    menu toggler icon and scroll to top function added!

--- src/public/js/navigation.js
+++ src/public/js/navigation.js
@@ -1,11 +1,22 @@
 const menuToggler = document.getElementById('menu-toggler');
 const nav = document.getElementById('nav');
-const navLinks = document.querySelectorAll('.nav_link')
+const navLinks = document.querySelectorAll('.nav_link');
+const toTop = document.getElementById('toTop');
+
 menuToggler.onclick = () => {
   nav.classList.toggle('active')
 }
+
 for(let navLink of navLinks) {
   navLink.onclick = () => {
     nav.classList.toggle('active')
   }
 }
+
+toTop.onclick = (event) => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: 'smooth'
+  });
+} 
