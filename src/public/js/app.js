1aa1ddf18bb3e099df8c1e1da39d1f1199ac0278
Árpád <developedbybarley@gmail.com>
2022-06-09 19:23:55 +0200

    Headline and basic menu toggler is added

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,11 @@
-console.log('Hello JS')
+const menuToggler = document.getElementById('menu-toggler');
+const nav = document.getElementById('nav');
+const navLinks = document.querySelectorAll('.nav_link')
+menuToggler.onclick = () => {
+  nav.classList.toggle('active')
+}
+for(let navLink of navLinks) {
+  navLink.onclick = () => {
+    nav.classList.toggle('active')
+  }
+}
