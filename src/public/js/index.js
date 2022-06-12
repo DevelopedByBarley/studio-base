a73124872ca4930188ee22e996aecfe63c7c9e50
Árpád <developedbybarley@gmail.com>
2022-06-12 14:29:47 +0200

    new initial commit

---
+++ src/public/js/index.js
@@ -0,0 +1,25 @@
+const menuToggler = document.getElementById('menu-toggler');
+const nav = document.getElementById('nav');
+const navLinks = document.querySelectorAll('.nav_link');
+const toTop = document.getElementById('toTop');
+const headerImg = document.getElementById('header-img');
+
+menuToggler.onclick = () => {
+  nav.classList.toggle('active')
+}
+
+for (let navLink of navLinks) {
+  navLink.onclick = () => {
+    nav.classList.toggle('active')
+  }
+}
+
+toTop.onclick = (event) => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: 'smooth'
+  });
+}
+
+
